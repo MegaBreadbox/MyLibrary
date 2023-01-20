391f4942af497f34dc78ec870cece583c0242d23
Joshua <joshuagutierrez724@gmail.com>
2023-01-20 18:31:36 -0500

    Recycler view added, and a single book is available. Currently using Glide and Materials 3

--- app/src/main/java/com/example/mylibrary/AllBooksActivity.java
+++ app/src/main/java/com/example/mylibrary/AllBooksActivity.java
@@ -1,14 +1,31 @@
 package com.example.mylibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class AllBooksActivity extends AppCompatActivity {
 
+    private RecyclerView booksRecView;
+    private BookRecViewAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_books);
+
+        adapter = new BookRecViewAdapter(this);
+        booksRecView = findViewById(R.id.booksRecView);
+
+        booksRecView.setAdapter(adapter);
+        booksRecView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book(1, "1Q84", "Hakuri Murakami", 1350, "https://cdn2.penguin.com.au/covers/original/9780099549062.jpg",
+                "A work of maddening brillance", "Insert Text"));
+        adapter.setBooks(books);
     }
 }
